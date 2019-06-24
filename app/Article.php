cbfc4fc7e9b12ee57ad0bdc65a565cc842a5ac1e
orionheidi <hajdidjukic@gmail.com>
2019-06-24 13:42:09 +0200

    4-relacije u modelu article, user i photo

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
 }
--- app/Photo.php
+++ app/Photo.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    //
+    public function article(){
+        return $this->belongsTo(Article::class);
+    }
 }
