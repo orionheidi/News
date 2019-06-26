9e027ac4685950227c1e37ceae1aa28c906c9a7e
orionheidi <hajdidjukic@gmail.com>
2019-06-26 17:03:42 +0200

    middlewear auth na create, da samo autentifikovan (logovan) user moze pristupiti toj stranici

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Auth::routes();
 Route::get('/allArticles','ArticleController@index')->name('all-articles');
 Route::get('/userArticles','UserController@index')->name('articles-by-user');
 Route::get('/singleArticle/{id}','ArticleController@show')->name('single-article');
-Route::get('/create', 'ArticleController@create')->name('create');
-Route::post('/create', 'ArticleController@store')->name('store');
+Route::get('/create', 'ArticleController@create')->name('create')->middleware('auth');
+Route::post('/create', 'ArticleController@store')->name('store')->middleware('auth');
 
 
